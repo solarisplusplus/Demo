9e0b8d1afa31c36d62b1be4706315118b3de4a06
Vasileios Panagiotidis <30659404+e-swissolar@users.noreply.github.com>
2019-10-04 21:46:16 +0200

    changes timestamp

--- src/js/app.js
+++ src/js/app.js
@@ -14,17 +14,16 @@ App = {
            App.web3Provider = web3.currentProvider;
          } else {
            //Create a new provider of the web3 object injected by our node
-           App.web3Provider = new Web3.providers.HttpProvider('http://167.71.47.69:42421'); 
-         }
-         web3 = new Web3(App.web3Provider);
+           App.web3Provider = new Web3.providers.HttpProvider('http://167.71.47.69:42421');
+         }         
+           web3 = new Web3(App.web3Provider);
 
          App.displayAccountInfo();
-
           return App.initContract();
      },
 
     displayAccountInfo: function(){
-      //Get balance of 0x004FE394a794DCd0b5D2B2FdA364CD9f22859D27
+      //Get balance of 0x004FE394a794DCd0b5D2B2FdA364CD9f22859D27 
       const account = '0x004FE394a794DCd0b5D2B2FdA364CD9f22859D27';
       $('#account').text(account)
       web3.eth.getBalance(account, function(err,balance){
@@ -38,6 +37,11 @@ App = {
         $('#BlockNumber').text(block);
         }   
       });
+      //Display timestamp of latest block as UTC
+      var datestamp = web3.eth.getBlock('latest').timestamp
+        date = new Date(datestamp*1000);      
+        time = date.toUTCString();
+        $('#Τimestamp').text(time);    
     },
 
      initContract: function() {
@@ -51,4 +55,4 @@ $(function() {
      $(window).load(function() {
           App.init();
      });
-});
+})
