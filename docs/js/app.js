1a62dc0d3b05eccc1164f503789c269ccab93c78
Vasileios Panagiotidis <30659404+e-swissolar@users.noreply.github.com>
2019-10-05 15:59:19 +0200

    changes timestamp with callback

--- docs/js/app.js
+++ docs/js/app.js
@@ -14,32 +14,16 @@ App = {
            App.web3Provider = web3.currentProvider;
          } else {
            //Create a new provider of the web3 object injected by our node
-           App.web3Provider = new Web3.providers.HttpProvider('http://167.71.47.69:42421'); 
-         }
-         web3 = new Web3(App.web3Provider);
+           App.web3Provider = new Web3.providers.HttpProvider('http://167.71.47.69:42421');
+         }         
+           web3 = new Web3(App.web3Provider);
 
          App.displayAccountInfo();
-
-
           return App.initContract();
      },
 
-    //  displayAccountInfo: function() {
-    //    web3.eth.getCoinbase(function(err, account){
-    //      if (err === null) {
-    //        App.account = account;
-    //        $('#account').text(account)
-    //        web3.eth.getBalance(account, function(err, balance){
-    //          if(err === null) {
-    //            $('#accountBalance').text(balance);   
-    //          }
-    //       })
-    //     }     
-    //   });
-    // },
-
     displayAccountInfo: function(){
-      //Get balance of 0x004FE394a794DCd0b5D2B2FdA364CD9f22859D27
+      //Get balance of 0x004FE394a794DCd0b5D2B2FdA364CD9f22859D27 
       const account = '0x004FE394a794DCd0b5D2B2FdA364CD9f22859D27';
       $('#account').text(account)
       web3.eth.getBalance(account, function(err,balance){
@@ -53,9 +37,17 @@ App = {
         $('#BlockNumber').text(block);
         }   
       });
+      //Display timestamp of latest block as UTC
+      web3.eth.getBlock('latest', function(err, result){
+        if (err===null) {
+        var datestamp = result.timestamp;
+        var date = new Date(datestamp*1000);
+        time = date.toUTCString();
+        $('#Τimestamp').text(time);
+        }
+      });
     },
-
-
+    
      initContract: function() {
           /*
            * Replace me...
@@ -67,4 +59,4 @@ $(function() {
      $(window).load(function() {
           App.init();
      });
-});
+})
